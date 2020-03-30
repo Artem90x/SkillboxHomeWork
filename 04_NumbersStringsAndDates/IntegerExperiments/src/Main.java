47ed7fdfcad46c1975cd978eb129fda3b06bb84d
Artem <artnicola25@gmail.com>
2020-03-30 15:27:53 +1000

    integer experements

--- 04_NumbersStringsAndDates/IntegerExperiments/src/Main.java
+++ 04_NumbersStringsAndDates/IntegerExperiments/src/Main.java
@@ -1,15 +1,19 @@
-public class Main
-{
-    public static void main(String[] args)
-    {
+public class Main {
+    public static void main(String[] args) {
         Container container = new Container();
         container.count += 7843;
-
+        System.out.println(sumDigits(222));
     }
 
-    public Integer sumDigits(Integer number)
-    {
+    public static Integer sumDigits(Integer number) {
         //@TODO: write code here
-        return 0;
+        int sum = 0;
+        Integer result = 0;
+
+        while (sum < Integer.toString(number).length()) {
+            result += (Integer.parseInt(String.valueOf(Integer.toString(number).charAt(sum))));
+            sum++;
+        }
+        return result;
     }
 }
