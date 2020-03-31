0d134a7f66e16d865a13112526946de3f29154a3
Artem <artnicola25@gmail.com>
2020-03-31 12:30:26 +1000

    min and max types of numbers

---
+++ 04_NumbersStringsAndDates/NewPoject/src/Main.java
@@ -0,0 +1,32 @@
+import java.math.BigDecimal;
+import java.text.DecimalFormat;
+
+public class Main {
+
+    public static void main(String[] args) {
+
+        System.out.println("Минимальное значение byte = " + Byte.MIN_VALUE);
+        System.out.println("Максимальное значение byte = " + Byte.MAX_VALUE +
+                "\n=============================\n");
+
+        System.out.println("Минимальное значение short = " + Short.MIN_VALUE);
+        System.out.println("Максимальное значение short = " + Short.MAX_VALUE +
+                "\n=============================\n");
+
+        System.out.println("Минимальное значение int = " + Integer.MIN_VALUE);
+        System.out.println("Максимальное значение int = " + Integer.MAX_VALUE +
+                "\n=============================\n");
+
+        System.out.println("Минимальное значение long = " + Long.MIN_VALUE);
+        System.out.println("Максимальное значение long = " + Long.MAX_VALUE +
+                "\n=============================\n");
+
+        System.out.println("Минимальное значение float = " + Float.MIN_VALUE);
+        System.out.println("Максимальное значение float = " + Float.MAX_VALUE +
+                "\n=============================\n");
+
+        System.out.println("Минимальное значение double = " + Double.MIN_VALUE);
+        System.out.println("Максимальное значение double = " + Double.MAX_VALUE +
+                "\n=============================\n");
+    }
+}
