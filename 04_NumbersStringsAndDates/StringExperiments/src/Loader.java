77f2da0b6b12c59b014f5095c7f9e8cba2202885
Artem <artnicola25@gmail.com>
2020-04-01 21:51:17 +1000

    conteiner

--- 04_NumbersStringsAndDates/StringExperiments/src/Loader.java
+++ 04_NumbersStringsAndDates/StringExperiments/src/Loader.java
@@ -1,9 +1,16 @@
 
-public class Loader
-{
-    public static void main(String[] args)
-    {
+public class Loader {
+
+    public static void main(String[] args) {
+
+        for (char i = 'A'; i <= 'Z'; i++) {
+            int code = i;
+            System.out.println(i + " :" + code);
+        }
+
         String text = "Вася заработал 5000 рублей, Петя - 7563 рубля, а Маша - 30000 рублей";
+
+
 
         System.out.println(text);
     }
