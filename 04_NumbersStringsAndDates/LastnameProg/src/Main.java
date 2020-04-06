628939dd4d9d9717dbe2e94c084fd8f91a1e130c
Artem <artnicola25@gmail.com>
2020-04-06 17:56:36 +1000

    edits progect lastname fistname program

--- 04_NumbersStringsAndDates/LastnameProg/src/Main.java
+++ 04_NumbersStringsAndDates/LastnameProg/src/Main.java
@@ -1,23 +1,34 @@
 import java.io.BufferedReader;
 import java.io.IOException;
 import java.io.InputStreamReader;
+import java.util.regex.Matcher;
+import java.util.regex.Pattern;
 
 public class Main {
 
     public static void main(String[] args) throws IOException {
         BufferedReader reader = new BufferedReader(new InputStreamReader(System.in));
         System.out.println("Напишите ваше ФИО:");
-        String fullName  = reader.readLine();
+        String fullName = reader.readLine();
 
         int lastIndex = fullName.indexOf(" ");
         int firstIndex = fullName.lastIndexOf(" ");
 
-        String lastName = fullName.substring(0,lastIndex);
-        String firstName = fullName.substring(lastIndex + 1, firstIndex);
-        String otherName = fullName.substring(firstIndex + 1);
+        Pattern p = Pattern.compile("\\d+");
+        Matcher m = p.matcher(fullName);
 
-        System.out.println("Фамилия: " + lastName);
-        System.out.println("Имя: " + firstName);
-        System.out.println("Отчество: " + otherName);
+        if (m.find()) {
+            System.out.println("Ошибка! Данные не соответствуют формату");
+        } else {
+            System.out.println("");
+
+            String lastName = fullName.substring(0, lastIndex);
+            String firstName = fullName.substring(lastIndex + 1, firstIndex);
+            String otherName = fullName.substring(firstIndex + 1);
+
+            System.out.println("Фамилия:" + lastName);
+            System.out.println("Имя:" + firstName);
+            System.out.println("Отчество:" + otherName);
+        }
     }
 }
