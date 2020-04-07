068121db3894bf88c6ea6398b59f01b1bffb5a73
Artem <artnicola25@gmail.com>
2020-04-07 14:33:14 +1000

    regex string experiments

--- 04_NumbersStringsAndDates/StringExperiments/src/Loader.java
+++ 04_NumbersStringsAndDates/StringExperiments/src/Loader.java
@@ -1,3 +1,5 @@
+import java.util.regex.Matcher;
+import java.util.regex.Pattern;
 
 public class Loader {
 
@@ -7,19 +9,27 @@ public class Loader {
             int code = i;
             System.out.println(i + " :" + code);
         }
-
         String text = "Вася заработал 5000 рублей, Петя - 7563 рубля, а Маша - 30000 рублей";
-
         System.out.println(text);
-        int a = text.indexOf("Вася заработал");
-        int b = text.indexOf("руб");
-        String cutOffVasya = text.substring(a + 14, b).trim();
 
-        int c = text.indexOf("Маша - ");
-        int d = text.lastIndexOf("руб");
-        String cutOffMasha = text.substring(c + 6, d).trim();
+//        int a = text.indexOf("Вася заработал");
+//        int b = text.indexOf("руб");
+//        String cutOffVasya = text.substring(a + 14, b).trim();
+//
+//        int c = text.indexOf("Маша - ");
+//        int d = text.lastIndexOf("руб");
+//        String cutOffMasha = text.substring(c + 6, d).trim();
+//
+//        int sum = Integer.parseInt(cutOffVasya) + Integer.parseInt(cutOffMasha);
+//        System.out.println("Сумма Васи и Маши: " + sum);
 
-        int sum = Integer.parseInt(cutOffVasya) + Integer.parseInt(cutOffMasha);
-        System.out.println("Сумма Васи и Маши: " + sum);
+        Pattern p = Pattern.compile("(\\d+)");
+        Matcher m = p.matcher(text);
+        int sum = 0;
+
+        while (m.find()) {
+            sum += Integer.parseInt(m.group());
+        }
+        System.out.println("Общая сумма: " + sum);
     }
 }
