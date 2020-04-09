fb0db6956cc5dbcd0215271ceb4b21b120d49a19
Artem <artnicola25@gmail.com>
2020-04-09 17:00:50 +1000

    program print age

--- 04_NumbersStringsAndDates/ProgramPrintAge/src/Main.java
+++ 04_NumbersStringsAndDates/ProgramPrintAge/src/Main.java
@@ -1,4 +1,27 @@
-package PACKAGE_NAME;
+import java.text.DateFormat;
+import java.text.SimpleDateFormat;
+import java.util.Calendar;
+import java.util.Date;
+import java.util.Locale;
 
 public class Main {
+
+    public static void main(String[] args) {
+        
+        DateFormat dateFormat = new SimpleDateFormat(" - dd.MM.yyyy - EEE", Locale.ENGLISH);
+        Calendar calendar = Calendar.getInstance();
+        calendar.set(1991, Calendar.JANUARY, 18);
+
+        Date date = new Date();
+        Date your = calendar.getTime();
+        int i = 0;
+
+        while (date.compareTo(your) > 0)
+        {
+            System.out.println(i + dateFormat.format(your));
+            calendar.add(Calendar.YEAR, 1);
+            your = calendar.getTime();
+            i++;
+        }
+    }
 }
