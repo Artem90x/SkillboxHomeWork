fd274a5832b14c167593d785377834ff4a477758
Artem <artnicola25@gmail.com>
2020-04-13 15:20:02 +1000

    repeat array creation color

--- 05_ArraysAndCollections/RepeatArrayCreation/src/Main.java
+++ 05_ArraysAndCollections/RepeatArrayCreation/src/Main.java
@@ -1,6 +1,12 @@
 public class Main {
 
     public static void main(String[] args) {
-        
+
+        String text = "Красный, Оранжевый, Желтый, Зеленый, Голубой, Синий, Фиолетовый";
+        String[] colors = text.split(",?\\s+");
+
+        for (int i = colors.length - 1; i >= 0; i--) {
+            System.out.println(colors[i]);
+        }
     }
 }
