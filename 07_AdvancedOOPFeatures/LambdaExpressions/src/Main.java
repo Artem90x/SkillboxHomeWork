ef7ce7d0330da03e37c66ec18e8c5f8b3d300a92
Artem <artnicola25@gmail.com>
2020-06-02 14:42:56 +1000

    stream lambda expressions

--- 07_AdvancedOOPFeatures/LambdaExpressions/src/Main.java
+++ 07_AdvancedOOPFeatures/LambdaExpressions/src/Main.java
@@ -10,7 +10,12 @@ public class Main {
             "\\LambdaExpressions\\data\\staff.txt";
     private static String dateFormat = "dd.MM.yyyy";
 
-    public static void main(String[] args) {
+    public static void main(String[] args) throws ParseException {
+        String beginningOfPeriod = "31.12.2016";
+        String endOfPeriod = "01.01.2018";
+        Date date1 = new SimpleDateFormat(dateFormat).parse(beginningOfPeriod);
+        Date date2 = new SimpleDateFormat(dateFormat).parse(endOfPeriod);
+
         ArrayList<Employee> staff = loadStaffFromFile();
         staff.sort((o1, o2) -> {
             int result = o1.getSalary().compareTo(o2.getSalary());
@@ -21,6 +26,11 @@ public class Main {
             }
         });
         staff.forEach(System.out::println);
+
+        Stream<Employee> stream = staff.stream();
+        stream.filter(e -> (e.getWorkStart().after(date1) & e.getWorkStart().before(date2)))
+                .max(Comparator.comparing(Employee::getSalary))
+                .ifPresent(e -> System.out.println("\nМаксимальная сумма зп в 2017 году: " + e.getSalary()));
     }
 
     private static ArrayList<Employee> loadStaffFromFile() {
