2a76fc75cd7f143ed13053a6043522bc83cf6765
Artem <artnicola25@gmail.com>
2020-06-08 13:39:59 +1000

    edit stream airport

--- 07_AdvancedOOPFeatures/StreamAirport/src/Main.java
+++ 07_AdvancedOOPFeatures/StreamAirport/src/Main.java
@@ -4,9 +4,11 @@ import com.skillbox.airport.Terminal;
 
 import java.text.SimpleDateFormat;
 import java.time.Instant;
+import java.util.Calendar;
 import java.util.Collection;
 import java.util.Comparator;
 import java.util.Date;
+import java.util.stream.Collectors;
 
 import static java.time.temporal.ChronoUnit.HOURS;
 
@@ -16,13 +18,25 @@ public class Main {
         Airport airport = Airport.getInstance();
         Date date = new Date();
         SimpleDateFormat dateFormat = new SimpleDateFormat("HH:mm");
+        Calendar calendar = Calendar.getInstance();
+        calendar.setTime(date);
+        calendar.add(Calendar.HOUR_OF_DAY, 2);
+        Date dataPlus2Hours = calendar.getTime();
 
         airport.getTerminals().stream()
-                .map(Terminal::getFlights)
-                .flatMap(Collection::stream).sorted(Comparator.comparing(Flight::getDate))
-                .filter(f -> f.getType().equals(Flight.Type.DEPARTURE)
-                        && f.getDate().getTime() <= Instant.now().plus(2, HOURS).toEpochMilli()
-                        && f.getDate().compareTo(date) > 0)
+                .flatMap(terminal -> terminal.getFlights().stream())
+                .collect(Collectors.toList())
+                .stream().filter(f -> (f.getType() == Flight.Type.ARRIVAL) &&
+                (f.getDate().after(date)) && f.getDate().before(dataPlus2Hours))
+                .sorted(Comparator.comparing(Flight::getDate))
                 .forEach(f -> System.out.println(dateFormat.format(f.getDate()) + "\t" + f.getAircraft().getModel()));
+
+//        airport.getTerminals().stream()
+//                .map(Terminal::getFlights)
+//                .flatMap(Collection::stream).sorted(Comparator.comparing(Flight::getDate))
+//                .filter(f -> f.getType().equals(Flight.Type.DEPARTURE)
+//                        && f.getDate().getTime() <= Instant.now().plus(2, HOURS).toEpochMilli()
+//                        && f.getDate().compareTo(date) > 0)
+//                .forEach(f -> System.out.println(dateFormat.format(f.getDate()) + "\t" + f.getAircraft().getModel()));
     }
 }
