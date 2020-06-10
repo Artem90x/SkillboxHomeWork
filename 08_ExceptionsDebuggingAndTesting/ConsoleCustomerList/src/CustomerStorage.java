ef204bfd7c6004c8e8257938daca58b1f4705bc8
Artem <artnicola25@gmail.com>
2020-06-10 17:01:55 +1000

    console customer list and add modul SPB metro

--- 08_ExceptionsDebuggingAndTesting/ConsoleCustomerList/src/CustomerStorage.java
+++ 08_ExceptionsDebuggingAndTesting/ConsoleCustomerList/src/CustomerStorage.java
@@ -12,8 +12,32 @@ public class CustomerStorage
     public void addCustomer(String data)
     {
         String[] components = data.split("\\s+");
+
+        if (components.length != 4) {
+            throw new IllegalArgumentException ("Wrong Format. Correct format:\n" +
+                    "\tadd Василий Петров vasily.petrov@gmail.com +79215637722");
+        }
+
+        if (!emailValid(components[2])) {
+            throw new IllegalArgumentException("Wrong Email Format. Correct format:\n" +
+                    "\tvasily.petrov@gmail.com");
+        }
+
+        if (!telephoneNumberValid(components[3])) {
+            throw new IllegalArgumentException("Wrong Telephone Number Format. Correct format:\n" +
+                    "\t+79215637722");
+        }
+
         String name = components[0] + " " + components[1];
         storage.put(name, new Customer(name, components[3], components[2]));
+    }
+
+    public boolean emailValid(String email) {
+        return email.matches("\\w+\\.?\\w+@\\w+\\.\\w{2,3}");
+    }
+
+    public boolean telephoneNumberValid(String telephoneNumber) {
+        return telephoneNumber.matches("^\\+\\d{11}$");
     }
 
     public void listCustomers()
--- 08_ExceptionsDebuggingAndTesting/ConsoleCustomerList/src/Main.java
+++ 08_ExceptionsDebuggingAndTesting/ConsoleCustomerList/src/Main.java
@@ -15,28 +15,26 @@ public class Main
         Scanner scanner = new Scanner(System.in);
         CustomerStorage executor = new CustomerStorage();
         for(;;)
-        {
-            String command = scanner.nextLine();
-            String[] tokens = command.split("\\s+", 2);
-            if(tokens[0].equals("add")) {
-                executor.addCustomer(tokens[1]);
-            }
-            else if(tokens[0].equals("list")) {
-                executor.listCustomers();
-            }
-            else if(tokens[0].equals("remove"))
+        try {
             {
-                executor.removeCustomer(tokens[1]);
+                String command = scanner.nextLine();
+                String[] tokens = command.split("\\s+", 2);
+                if (tokens[0].equals("add")) {
+                    executor.addCustomer(tokens[1]);
+                } else if (tokens[0].equals("list")) {
+                    executor.listCustomers();
+                } else if (tokens[0].equals("remove")) {
+                    executor.removeCustomer(tokens[1]);
+                } else if (tokens[0].equals("count")) {
+                    System.out.println("There are " + executor.getCount() + " customers");
+                } else if (tokens[0].equals("help")) {
+                    System.out.println(helpText);
+                } else {
+                    System.out.println(commandError);
+                }
             }
-            else if(tokens[0].equals("count")) {
-                System.out.println("There are " + executor.getCount() + " customers");
-            }
-            else if(tokens[0].equals("help")) {
-                System.out.println(helpText);
-            }
-            else {
-                System.out.println(commandError);
-            }
+        } catch (IllegalArgumentException e) {
+            System.out.println(e.getMessage());
         }
     }
 }
