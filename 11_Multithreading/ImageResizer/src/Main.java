950deb0a38c5ad55b2c0ee82bbcc12e7f5fa9b2e
Artem <artnicola25@gmail.com>
2020-08-28 19:17:16 +1000

    add 11 module and creating work ImageResizer

--- 11_Multithreading/ImageResizer/src/Main.java
+++ 11_Multithreading/ImageResizer/src/Main.java
@@ -1,56 +1,31 @@
-import javax.imageio.ImageIO;
-import java.awt.image.BufferedImage;
 import java.io.File;
+import java.util.ArrayList;
+import java.util.List;
 
-public class Main
-{
-    public static void main(String[] args)
-    {
-        String srcFolder = "/users/sortedmap/Desktop/src";
-        String dstFolder = "/users/sortedmap/Desktop/dst";
+public class Main {
+    private static final int newWidth = 300;
+    private static final int CORES = 2;
+    private static final String srcFolder = "C:/Users/User/IdeaProjects/java_basics/" +
+            "11_Multithreading/ImageResizer/data/src";
+    private static final String dstFolder = "C:/Users/User/IdeaProjects/java_basics/" +
+            "11_Multithreading/ImageResizer/data/dst";
+
+    public static void main(String[] args) {
 
         File srcDir = new File(srcFolder);
-
         long start = System.currentTimeMillis();
 
         File[] files = srcDir.listFiles();
+        assert files != null;
+        int middle = files.length / CORES;
 
-        try
-        {
-            for(File file : files)
-            {
-                BufferedImage image = ImageIO.read(file);
-                if(image == null) {
-                    continue;
-                }
+        List<ImageResizer> imageResizers = new ArrayList<>();
 
-                int newWidth = 300;
-                int newHeight = (int) Math.round(
-                        image.getHeight() / (image.getWidth() / (double) newWidth)
-                );
-                BufferedImage newImage = new BufferedImage(
-                        newWidth, newHeight, BufferedImage.TYPE_INT_RGB
-                );
-
-                int widthStep = image.getWidth() / newWidth;
-                int heightStep = image.getHeight() / newHeight;
-
-                for (int x = 0; x < newWidth; x++)
-                {
-                    for (int y = 0; y < newHeight; y++) {
-                        int rgb = image.getRGB(x * widthStep, y * heightStep);
-                        newImage.setRGB(x, y, rgb);
-                    }
-                }
-
-                File newFile = new File(dstFolder + "/" + file.getName());
-                ImageIO.write(newImage, "jpg", newFile);
-            }
+        for (int i = 0; i < CORES; i++) {
+            File[] threadFiles = new File[middle];
+            System.arraycopy(files, middle * i, threadFiles, 0, threadFiles.length);
+            imageResizers.add(i, new ImageResizer(threadFiles, newWidth, dstFolder, start));
+            new Thread(imageResizers.get(i)).start();
         }
-        catch (Exception ex) {
-            ex.printStackTrace();
-        }
-
-        System.out.println("Duration: " + (System.currentTimeMillis() - start));
     }
 }
