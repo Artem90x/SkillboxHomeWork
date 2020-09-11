fcbfdffa16010007a665e5922f563e6941b7e917
Artem <artnicola25@gmail.com>
2020-09-11 21:48:25 +1000

    transactions project

--- 11_Multithreading/Transactions/src/main/java/Bank.java
+++ 11_Multithreading/Transactions/src/main/java/Bank.java
@@ -1,16 +1,18 @@
 import java.util.HashMap;
 import java.util.Random;
 
-public class Bank
-{
+public class Bank {
     private HashMap<String, Account> accounts;
-    private final Random random = new Random();
+    private final Random RANDOM = new Random();
+
+    public Bank(HashMap<String, Account> accounts) {
+        this.accounts = accounts;
+    }
 
     public synchronized boolean isFraud(String fromAccountNum, String toAccountNum, long amount)
-        throws InterruptedException
-    {
+            throws InterruptedException {
         Thread.sleep(1000);
-        return random.nextBoolean();
+        return RANDOM.nextBoolean();
     }
 
     /**
@@ -20,16 +22,38 @@ public class Bank
      * метод isFraud. Если возвращается true, то делается блокировка
      * счетов (как – на ваше усмотрение)
      */
-    public void transfer(String fromAccountNum, String toAccountNum, long amount)
-    {
+    public void transfer(String fromAccountNum, String toAccountNum, long amount) throws InterruptedException {
+        Account accFrom = accounts.get(fromAccountNum);
+        Account accTo = accounts.get(toAccountNum);
+        if (accFrom.isBlocked()) {
+            System.out.println("Account " + accFrom.getAccNumber() + " is blocked! Transfer denied!");
+            return;
+        }
+        if (accTo.isBlocked()) {
+            System.out.println("Account " + accTo.getAccNumber() + " is blocked! Transfer denied!");
+            return;
+        }
+        synchronized (Integer.parseInt(accFrom.getAccNumber()) < Integer.parseInt(accTo.getAccNumber()) ?
+                accFrom : accTo) {
+            synchronized (Integer.parseInt(accFrom.getAccNumber()) > Integer.parseInt(accTo.getAccNumber()) ?
+                    accFrom : accTo) {
+                accFrom.setMoney(accFrom.getMoney() - amount);
+                accTo.setMoney(accTo.getMoney() + amount);
 
+                if (amount > 50000) {
+                    boolean status = isFraud(fromAccountNum, toAccountNum, amount);
+                    accFrom.setBlocked(status);
+                    accTo.setBlocked(status);
+                }
+            }
+        }
     }
 
     /**
      * TODO: реализовать метод. Возвращает остаток на счёте.
      */
-    public long getBalance(String accountNum)
-    {
-        return 0;
+    public long getBalance(String accountNum) {
+        Account account = accounts.get(accountNum);
+        return account.getMoney();
     }
 }
---
+++ 11_Multithreading/Transactions/src/main/java/Main.java
@@ -0,0 +1,56 @@
+import java.util.ArrayList;
+import java.util.HashMap;
+
+public class Main {
+
+    private static final int THREAD_COUNT = 4;
+
+    public static void main(String[] args) throws InterruptedException {
+
+        HashMap<String, Account> accounts = new HashMap<>();
+        for (int i = 1; i <= 100; i++) {
+            String accNumber = String.valueOf(i);
+            long money = Math.round(100000 * Math.random());
+            Account acc = new Account();
+            acc.setAccNumber(accNumber);
+            acc.setMoney(money);
+            accounts.put(accNumber, acc);
+        }
+        Bank bank = new Bank(accounts);
+
+        long beforeBalance = 0;
+        for (int j = 1; j <= accounts.size(); j++) {
+            beforeBalance += bank.getBalance(String.valueOf(j));
+        }
+        System.out.println("Баланс до переводов: " + beforeBalance);
+
+        ArrayList<Thread> threads = new ArrayList<>();
+        for (int k = 0; k < THREAD_COUNT; k++) {
+            int threadNumber = k + 1;
+            threads.add(new Thread(() -> {
+                long start = System.currentTimeMillis();
+                long finish = System.currentTimeMillis() - start;
+                int anyNumber = (int) Math.round(99 * Math.random());
+                String accFrom = String.valueOf(anyNumber);
+                String accTo = String.valueOf(anyNumber + 1);
+                long amount = (long) (50000 * Math.random());
+                try {
+                    bank.transfer(accFrom, accTo, amount);
+                } catch (InterruptedException e) {
+                    e.printStackTrace();
+                }
+                System.out.println("Поток " + threadNumber + " окончен: " + finish + " ms");
+            }));
+        }
+        threads.forEach(Thread::start);
+        for (Thread thread : threads) {
+            thread.join();
+        }
+
+        long afterBalance = 0;
+        for (int n = 1; n <= accounts.size(); n++) {
+            afterBalance += bank.getBalance(String.valueOf(n));
+        }
+        System.out.println("Баланс после переводов: " + afterBalance);
+    }
+}
